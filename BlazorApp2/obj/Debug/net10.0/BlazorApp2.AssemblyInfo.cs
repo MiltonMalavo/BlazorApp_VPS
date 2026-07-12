7b816da19869ac969ff65660b19265dc06d4bf3a
miltonmalavo <miltonmalavo75@gmail.com>
2026-07-12 22:15:45 +0100

    eded

--- BlazorApp2/obj/Debug/net10.0/BlazorApp2.AssemblyInfo.cs
+++ BlazorApp2/obj/Debug/net10.0/BlazorApp2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9a7f591fba0cfa78030673fb5480e9accc6a76d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
